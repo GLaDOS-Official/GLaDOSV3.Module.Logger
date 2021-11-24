c641d1075ba567c455ae8db17340be6d2f523e0e
BlackOfWorld <admin@blackofworld.fun>
2021-11-24 22:16:58 +0100

    Implemention of functions

--- LoggerService.cs
+++ LoggerService.cs
@@ -3,9 +3,15 @@ using Discord.Rest;
 using Discord.WebSocket;
 using GLaDOSV3.Helpers;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
 
 namespace GLaDOSV3.Module.Logger
 {
@@ -17,35 +23,58 @@ namespace GLaDOSV3.Module.Logger
         {
             discord.UserJoined += this.DiscordOnUserJoined;
             discord.UserLeft += this.DiscordOnUserLeft;
-            //discord.ChannelDestroyed += this.DiscordChannelDestroyed;
-            //discord.RoleCreated += this.DiscordRoleCreated;
-            //discord.RoleDeleted += this.DiscordRoleDeleted;
-            //discord.RoleUpdated += this.DiscordRoleUpdated;
-            //discord.GuildUpdated += this.DiscordGuildUpdated;
-            //discord.UserBanned += this.DiscordUserBanned;
-            //discord.UserUnbanned += this.DiscordUserUnbanned;
-            //discord.GuildMemberUpdated += this.DiscordGuildMemberUpdated;
-            //discord.UserVoiceStateUpdated += this.DiscordUserVoiceStateUpdated;
-            //discord.ReactionsCleared += this.DiscordReactionsCleared;
+            discord.ChannelDestroyed += this.DiscordChannelDestroyed;
+            discord.RoleCreated += this.DiscordRoleCreated;
+            discord.RoleDeleted += this.DiscordRoleDeleted;
+            discord.RoleUpdated += this.DiscordRoleUpdated;
+            discord.GuildUpdated += this.DiscordGuildUpdated;
+            discord.UserBanned += this.DiscordUserBanned;
+            discord.UserUnbanned += this.DiscordUserUnbanned;
+            discord.GuildMemberUpdated += this.DiscordGuildMemberUpdated;
             //discord.MessageDeleted += this.DiscordMessageDeleted;
             //discord.MessageUpdated += this.DiscordMessageUpdated;
-            //discord.ChannelCreated += this.DiscordChannelCreated;
+            discord.ChannelCreated += this.DiscordChannelCreated;
             //discord.InviteDeleted += this.DiscordInviteDeleted;
             //discord.InviteCreated += this.DiscordInviteCreated;
             //discord.MessagesBulkDeleted += this.DiscordMessagesBulkDeleted;
-            //discord.ChannelUpdated += this.DiscordChannelUpdated;
+            discord.ChannelUpdated += this.DiscordChannelUpdated;
         }
 
-        //private Task DiscordChannelUpdated(SocketChannel beforeChannel, SocketChannel afterChannel)
-        //{
-        //    throw new NotImplementedException();
-        //}
+        private async Task DiscordChannelUpdated(SocketChannel beforeChannel, SocketChannel afterChannel)
+        {
+            if (afterChannel is not SocketGuildChannel channel) return;
 
-        //private Task DiscordMessagesBulkDeleted(IReadOnlyCollection<Cacheable<IMessage, ulong>> cachedMessages, Cacheable<IMessageChannel, ulong> channel)
-        //{
-        //    throw new NotImplementedException();
-        //}
+            var builder = new EmbedBuilder();
+            builder.AddField("Name", channel.Name);
+            builder.AddField("Id", $"```ini\nUser = Unknown\nChannel = {channel.Id}```");
+            builder.AddField("Creation date", channel.CreatedAt);
+            builder.WithAuthor("Unknown", "https://discordapp.com/assets/1cbd08c76f8af6dddce02c5138971129.png");
+            var logs = await channel.Guild.GetAuditLogsAsync(15, null, null, null, ActionType.ChannelCreated).FlattenAsync();
+            var log = logs.FirstOrDefault(a => ((ChannelUpdateAuditLogData)a.Data).ChannelId == channel.Id);
+            if (log == null) return;
+            if (DateTime.UtcNow.Ticks - log.CreatedAt.UtcTicks > 3000) return;
+            builder.WithAuthor(log.User);
+            builder.Fields[1].Value = $"```ini\nUser = {log.User.Id}\nChannel = {channel.Id}```";
+            try { }
+            finally { builder.Build(); }
+        }
 
+        //TODO: FINISH THIS YOU LAZY FUCK
+        // https://github.com/curtisf/logger/blob/master/src/bot/events/messageDeleteBulk.js
+#if false
+        private async Task DiscordMessagesBulkDeleted(IReadOnlyCollection<Cacheable<IMessage, ulong>> cachedMessages, Cacheable<IMessageChannel, ulong> channel)
+        {
+            string fuck = "";
+            foreach (Cacheable<IMessage, ulong> cachedMessage in cachedMessages)
+            {
+                if (!cachedMessage.HasValue) return;
+                var    value = cachedMessage.Value;
+                fuck  = $"<{value.Author.Username}#{value.Author.Discriminator} ({value.Author.Id})> {value.Content}\n";
+            }
+         
+
+        }
+#endif
         //private Task DiscordInviteCreated(SocketInvite invite)
         //{
         //    throw new NotImplementedException();
@@ -60,7 +89,13 @@ namespace GLaDOSV3.Module.Logger
         {
             if (createdChannel is not SocketGuildChannel channel) return;
 
-            var builder = new EmbedBuilder();
+            var name = channel.GetType().Name;
+            name = name.Split("Socket")[1];
+            name = name.Insert(name.IndexOf("Channel", StringComparison.Ordinal), " ");
+            var builder = new EmbedBuilder
+            {
+                Description = $"{name} created <#{channel.Id}>"
+            };
             builder.AddField("Name", channel.Name);
             builder.AddField("Id", $"```ini\nUser = Unknown\nChannel = {createdChannel.Id}```");
             builder.WithAuthor("Unknown", "https://discordapp.com/assets/1cbd08c76f8af6dddce02c5138971129.png");
@@ -84,55 +119,217 @@ namespace GLaDOSV3.Module.Logger
         //    throw new NotImplementedException();
         //}
 
-        //private Task DiscordReactionsCleared(Cacheable<IUserMessage, ulong> cachedMessage, Cacheable<IMessageChannel, ulong> channel)
-        //{
-        //    throw new NotImplementedException();
-        //}
-
         //private Task DiscordUserVoiceStateUpdated(SocketUser user, SocketVoiceState before, SocketVoiceState after)
         //{
         //    throw new NotImplementedException();
         //}
 
-        //private Task DiscordGuildMemberUpdated(Cacheable<SocketGuildUser, ulong> before, SocketGuildUser after)
-        //{
-        //    throw new NotImplementedException();
-        //}
+        private async Task DiscordGuildMemberUpdated(Cacheable<SocketGuildUser, ulong> before, SocketGuildUser after)
+        {
+            if (!before.HasValue) return;
+            var builder = new EmbedBuilder
+            {
+                Description = $"{after} {after.Mention} {(string.IsNullOrWhiteSpace(after.Nickname) ? "" : $"({after.Nickname})")} was updated"
+            };
+            builder.WithAuthor(after);
+            var fuck = StaticTools.Compare(before.Value, after);
+            var output = fuck.Select(field => field.Split(" ")).Aggregate("", (current, split) => current + $"Variable: {split[0]}\nOld: {split[1]}\nNew: {split[2]}");
+            if (string.IsNullOrWhiteSpace(output)) return; // nothing changed?
+            builder.WithAuthor("Unknown", "https://discordapp.com/assets/1cbd08c76f8af6dddce02c5138971129.png");
+            var logs = await after.Guild.GetAuditLogsAsync(15, null, null, null, ActionType.MemberUpdated).FlattenAsync();
+            var log = logs.FirstOrDefault(a => ((MemberUpdateAuditLogData)a.Data).Target.Id == after.Id);
+            if (log                                            == null) return;
+            if (DateTime.UtcNow.Ticks - log.CreatedAt.UtcTicks > 3000) return;
+            builder.WithAuthor(log.User);
+            try { }
+            finally { builder.Build(); }
+        }
 
-        //private Task DiscordUserUnbanned(SocketUser user, SocketGuild server)
-        //{
-        //    throw new NotImplementedException();
-        //}
+        private async Task DiscordUserUnbanned(SocketUser user, SocketGuild server)
+        {
+            var builder = new EmbedBuilder
+            {
+                Description = $"{user} was unbanned"
+            };
+            builder.AddField("User Information", $"{user.Username}#{user.Discriminator} ({user.Id}) {user.Mention} {(user.IsBot ? "(Bot)" : "")}");
+            builder.AddField("Reason", "None provided");
+            builder.AddField("Id", $"```ini\nUser = {user.Id}\nStaff member = Unknown```");
+            builder.WithAuthor(user);
+            await Task.Delay(1000);
+            var logs = await server.GetAuditLogsAsync(15, null, null, null, ActionType.Unban).FlattenAsync();
+            var log  = logs.FirstOrDefault(a => ((UnbanAuditLogData)a.Data).Target.Id == user.Id);
+            if (log                                            == null) return;
+            if (DateTime.UtcNow.Ticks - log.CreatedAt.UtcTicks > 3000) return;
 
-        //private Task DiscordUserBanned(SocketUser user, SocketGuild server)
-        //{
-        //    throw new NotImplementedException();
-        //}
+            if (!string.IsNullOrWhiteSpace(log.Reason)) builder.Fields[1].Value = log.Reason;
+            builder.Fields[2].Value = $"```ini\nUser = {user.Id}\nStaff member = {log.User.Id}```";
+            builder.WithFooter(log.User.ToString(),
+                               string.IsNullOrWhiteSpace(log.User.AvatarId)
+                                   ? log.User.GetDefaultAvatarUrl()
+                                   : log.User.GetAvatarUrl());
+            try { }
+            finally { builder.Build(); }
+        }
 
-        //private Task DiscordGuildUpdated(SocketGuild before, SocketGuild after)
-        //{
-        //    throw new NotImplementedException();
-        //}
+        private async Task DiscordUserBanned(SocketUser user, SocketGuild server)
+        {
+            var builder = new EmbedBuilder
+            {
+                Description = $"{user} was banned"
+            };
+            builder.AddField("User Information",$"{user.Username}#{user.Discriminator} ({user.Id}) {user.Mention} {(user.IsBot ? "(Bot)" : "")}");
+            builder.AddField("Reason", "None provided");
+            builder.AddField("Id", $"```ini\nUser = {user.Id}\nStaff member = Unknown```");
+            builder.WithAuthor(user);
+            await Task.Delay(1000);
+            var logs = await server.GetAuditLogsAsync(15, null, null, null, ActionType.Ban).FlattenAsync();
+            var log = logs.FirstOrDefault(a => ((BanAuditLogData)a.Data).Target.Id == user.Id);
+            if (log                                            == null) return;
+            if (DateTime.UtcNow.Ticks - log.CreatedAt.UtcTicks > 3000) return;
 
-        //private Task DiscordRoleUpdated(SocketRole before, SocketRole after)
-        //{
-        //    throw new NotImplementedException();
-        //}
+            if(!string.IsNullOrWhiteSpace(log.Reason)) builder.Fields[1].Value = log.Reason;
+            builder.Fields[2].Value = $"```ini\nUser = {user.Id}\nStaff member = {log.User.Id}```";
+            builder.WithFooter(log.User.ToString(),
+                               string.IsNullOrWhiteSpace(log.User.AvatarId)
+                                   ? log.User.GetDefaultAvatarUrl()
+                                   : log.User.GetAvatarUrl());
+            try { }
+            finally { builder.Build(); }
+        }
 
-        //private Task DiscordRoleDeleted(SocketRole role)
-        //{
-        //    throw new NotImplementedException();
-        //}
+        private async Task DiscordGuildUpdated(SocketGuild before, SocketGuild after)
+        {
+            var builder = new EmbedBuilder
+            {
+                Description = $"Server {after} was modifed"
+            };
 
-        //private Task DiscordRoleCreated(SocketRole role)
-        //{
-        //    throw new NotImplementedException();
-        //}
+            var fuck = StaticTools.Compare(before, after);
+            var output = fuck.Select(field => field.Split(" ")).Aggregate("", (current, split) => current + $"Variable: {split[0]}\nOld: {split[1]}\nNew: {split[2]}");
+            if (string.IsNullOrWhiteSpace(output)) return; // nothing changed?
 
-        //private Task DiscordChannelDestroyed(SocketChannel channel)
-        //{
-        //    throw new NotImplementedException();
-        //}
+            var logs = await before.GetAuditLogsAsync(15, null, null, null, ActionType.GuildUpdated).FlattenAsync();
+            var log = logs.First();
+            if (log                                            == null) return;
+            if (DateTime.UtcNow.Ticks - log.CreatedAt.UtcTicks > 3000) return;
+            builder.WithAuthor(log.User);
+            builder.AddField("Id", $"```ini\nUser = {log.User.Id}\nRole = {after.Id}```");
+            try { }
+            finally { if (builder.Fields.Count != 0) builder.Build(); }
+        }
+
+        private async Task DiscordRoleUpdated(SocketRole before, SocketRole after)
+        {
+            var builder = new EmbedBuilder
+            {
+                Description = $"Role {after} was modified"
+            };
+
+            var fuck = StaticTools.Compare(before, after);
+            var output = fuck.Select(field => field.Split(" ")).Aggregate("", (current, split) => current + $"Variable: {split[0]}\nOld: {split[1]}\nNew: {split[2]}");
+            if (string.IsNullOrWhiteSpace(output)) return; // nothing changed?
+
+            var logs = await before.Guild.GetAuditLogsAsync(15, null, null, null, ActionType.RoleUpdated).FlattenAsync();
+            var log = logs.FirstOrDefault(a => ((RoleUpdateAuditLogData)a.Data).RoleId == after.Id);
+            if (log == null) return;
+            if (DateTime.UtcNow.Ticks - log.CreatedAt.UtcTicks > 3000) return;
+            builder.WithAuthor(log.User);
+            builder.AddField("Id", $"```ini\nUser = {log.User.Id}\nRole = {after.Id}```");
+            try { }
+            finally { if (builder.Fields.Count != 0) builder.Build(); }
+        }
+
+        private async Task DiscordRoleDeleted(SocketRole role)
+        {
+            var builder = new EmbedBuilder
+            {
+                Description = $"Role {role} was deleted"
+            };
+            builder.AddField("Name", role.Name);
+            builder.AddField("Id", $"```ini\nUser = Unknown\nRole = {role.Id}```");
+            builder.AddField("Reason", "None.");
+            builder.WithAuthor("Unknown", "https://discordapp.com/assets/1cbd08c76f8af6dddce02c5138971129.png");
+
+            var logs = await role.Guild.GetAuditLogsAsync(15, null, null, null, ActionType.RoleDeleted).FlattenAsync();
+            var log = logs.FirstOrDefault(a => ((RoleDeleteAuditLogData)a.Data).RoleId == role.Id);
+            if (log == null) return;
+            if (DateTime.UtcNow.Ticks - log.CreatedAt.UtcTicks > 3000) return;
+            builder.WithAuthor(log.User);
+            builder.Fields[1].Value = $"```ini\nUser = {log.User.Id}\nRole = {role.Id}```";
+            builder.Fields[2].Value = log.Reason;
+            try { }
+            finally { builder.Build(); }
+        }
+
+        private async Task DiscordRoleCreated(SocketRole role)
+        {
+            var builder = new EmbedBuilder
+            {
+                Description = $"Role {role} was created"
+            };
+            builder.AddField("Name", role.Name);
+            builder.AddField("Id", $"```ini\nUser = Unknown\nRole = {role.Id}```");
+            builder.AddField("Type", "User", true);
+            builder.WithAuthor("Unknown", "https://discordapp.com/assets/1cbd08c76f8af6dddce02c5138971129.png");
+            var a = await role.Guild.GetUsersAsync().FlattenAsync();
+            if (!a.Any(b => b.Username == role.Name))
+            {
+                if (role.IsManaged && a.Any(b => b.Username == role.Name)) builder.Fields[2].Value = "Bot";
+
+                var logs = await role.Guild.GetAuditLogsAsync(15, null, null, null, ActionType.RoleCreated).FlattenAsync();
+                var log = logs.FirstOrDefault(a => ((RoleCreateAuditLogData)a.Data).RoleId == role.Id);
+                if (log == null) return;
+                if (DateTime.UtcNow.Ticks - log.CreatedAt.UtcTicks > 3000) return;
+                builder.WithAuthor(log.User);
+                builder.Fields[1].Value = $"```ini\nUser = {log.User.Id}\nRole = {role.Id}```";
+            }
+            else
+                builder.Fields[1].Value = $"```ini\nUser = *created by invite*\nRole = {role.Id}```";
+
+            try { }
+            finally { builder.Build(); }
+        }
+
+        private async Task DiscordChannelDestroyed(SocketChannel chan)
+        {
+            if (chan is not SocketGuildChannel channel) return;
+
+            var name = channel.GetType().Name;
+            name = name.Split("Socket")[1];
+            name = name.Insert(name.IndexOf("Channel", StringComparison.Ordinal), " ");
+            var builder = new EmbedBuilder
+            {
+                Description = $"{name} deleted <#{channel.Id}>"
+            };
+            builder.AddField("Name", channel.Name);
+            builder.AddField("Id", $"```ini\nUser = Unknown\nChannel = {channel.Id}```");
+            builder.AddField("Creation date", channel.CreatedAt, true);
+            builder.AddField("Position", channel.Position, true);
+            builder.WithAuthor("Unknown", "https://discordapp.com/assets/1cbd08c76f8af6dddce02c5138971129.png");
+
+            if (channel.PermissionOverwrites.Count != 0)
+            {
+                foreach (var permission in channel.PermissionOverwrites)
+                {
+                    if (permission.TargetType != PermissionTarget.Role) continue;
+                    var role = channel.Guild.GetRole(permission.TargetId);
+                    if (role.Name == "@everyone") return;
+                    string output =
+                        $"Allow: {string.Join(", ", permission.Permissions.ToAllowList())}\nDeny: {string.Join(", ", permission.Permissions.ToDenyList())}";
+
+                    builder.AddField("Permissions of "+role.Name, output);
+                }
+            }
+
+            var logs = await channel.Guild.GetAuditLogsAsync(15, null, null, null, ActionType.ChannelDeleted).FlattenAsync();
+            var log = logs.FirstOrDefault(a => ((ChannelDeleteAuditLogData)a.Data).ChannelId == channel.Id);
+            if (log == null) return;
+            if (DateTime.UtcNow.Ticks - log.CreatedAt.UtcTicks > 3000) return;
+            builder.WithAuthor(log.User);
+            builder.Fields[1].Value = $"```ini\nUser = {log.User.Id}\nChannel = {channel.Id}```";
+            try { }
+            finally { builder.Build(); }
+        }
 
         private async Task DiscordOnUserJoined(SocketGuildUser socketGuildUser)
         {
